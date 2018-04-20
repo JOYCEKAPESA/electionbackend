214abedaa1efd23fed671e566730ad179f610636
JOYCEKAPESA <34416433+JOYCEKAPESA@users.noreply.github.com>
2018-04-21 00:26:32 +0300

    vote

--- api.php
+++ api.php
@@ -33,24 +33,30 @@ switch ($action) {
         echo json_encode($response); // echo login status
         break;
 
-    case 'vote':
+    case 'vote_sheet':
         //Generating a list of candidates so one can vote
-        $query_vote_sheet = "SELECT first_name, last_name, position_name
+        $query_vote_sheet = "SELECT c.id AS candId, first_name, last_name, position_name
                                FROM candidates c
                          INNER JOIN positions p
                                  ON p.id = c.position_id
                          INNER JOIN students s 
-                                 ON s.id = c.student_id"; 
+                                 ON s.id = c.student_id";
 
         $result_vote_sheet = mysqli_query($link, $query_vote_sheet) or die(mysqli_error($link));
 
         while ($row = mysqli_fetch_array($result_vote_sheet)) {
-            $candidates[$row['position_name']][] = $row['first_name'] . " " . $row['last_name']; //Dynamically creating associative array
+            $candidates[$row['position_name']][] = array(
+                "full_name" => $row['first_name'] . " " . $row['last_name'],
+                "candidate_id" => $row["candId"]
+            );
         }
 
         echo json_encode($candidates); // Creating json response for voting sheet
         break;
-       
+
+    case 'cast_votes':
+        prinr($_GET['presidents']);
+        break;
 }
 ?>
 
