e3b280a7f9ad0e04a00a13e573b048f1f1427679
JOYCEKAPESA <34416433+JOYCEKAPESA@users.noreply.github.com>
2018-04-21 21:05:41 +0300

    adds cast votes feature

--- api.php
+++ api.php
@@ -55,7 +55,55 @@ switch ($action) {
         break;
 
     case 'cast_votes':
-        prinr($_GET['presidents']);
+
+        //Cast votes
+
+        $selectedPresident = $_GET['Presidents'];
+        $sselectedSenetor = $_GET['Senators'];
+        $selectedFr = $_GET['Faculty_Representatives'];
+
+//        print_r($_GET);
+
+        foreach ($_GET as $candidate_id) { //since number of votes which can be casted is dynamic so we have to loop na hizo id za candidates tunazipata kwenye hiyo for loop
+            if ($candidate_id !== 'cast_votes') {
+
+                //Get position id from provided candidate id
+                $query_get_candidate = "SELECT position_id
+                                          FROM candidates
+                                         WHERE id = {$candidate_id}
+                                         LIMIT 1";
+
+                $result_get_candidate = mysqli_query($link, $query_get_candidate) or die(mysqli_error($link));
+
+                if (mysqli_num_rows($result_get_candidate) == 1) {
+                    $row = mysqli_fetch_array($result_get_candidate);
+                    $position_id = $row['position_id'];
+
+                    //CAST A VOTE
+                    $query_cast_vote = "INSERT INTO votes
+                                                 (user_id, positon_id, election_period_id, candidate_id)
+                                          VALUES (5, '{$position_id}', 1, '{$candidate_id}')";
+
+                    $result_cast_vote = mysqli_query($link, $query_cast_vote) or die(mysqli_error($link));
+
+                    if ($result_cast_vote) {
+
+                        //Votes casted succefully 
+                        $response = array(
+                            'cast_status' => 'success'
+                        );
+                    } else {
+                        //Can't cast vots
+                        $response = array(
+                            'cast_status' => 'failed'
+                        );
+                    }
+                }
+            }
+        }
+
+        echo json_encode($response);
+
         break;
 }
 ?>
