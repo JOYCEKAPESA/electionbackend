ccb219039e1f868fb68ec6b49b32380a2e8663b8
JOYCEKAPESA <34416433+JOYCEKAPESA@users.noreply.github.com>
2018-05-02 01:03:41 +0300

    shows candidates depending on user login details

--- api.php
+++ api.php
@@ -46,24 +46,31 @@ switch ($action) {
         $course_id = $_GET['course_id'];
         $batch_id = $_GET['batch_id'];
         //Generating a list of candidates so one can vote
-        $query_vote_sheet = "SELECT c.id AS candId, first_name, last_name, position_name, c.faculty_id, c.course_id, c.batch_id
+        $query_vote_sheet = "SELECT c.id AS candId, first_name, last_name, position_name, 
+                                    c.faculty_id, c.course_id, c.batch_id, COUNT(IFNULL(candidate_id, 0)) AS num_votes
                                FROM candidates c
                          INNER JOIN positions p
                                  ON p.id = c.position_id
                          INNER JOIN students s 
                                  ON s.id = c.student_id
+                          LEFT JOIN votes v
+                                 ON v.candidate_id = c.id
                               WHERE (c.faculty_id = {$faculty_id} OR c.faculty_id IS NULL)
                                 AND (c.course_id = {$course_id} OR c.course_id IS NULL)
-                                AND (c.batch_id = {$batch_id} OR c.batch_id IS NULL)";
+                                AND (c.batch_id = {$batch_id} OR c.batch_id IS NULL)
+                           GROUP BY c.id";
 
         $result_vote_sheet = mysqli_query($link, $query_vote_sheet) or die(mysqli_error($link));
 
         while ($row = mysqli_fetch_array($result_vote_sheet)) {
             $candidates[$row['position_name']][] = array(
                 "full_name" => $row['first_name'] . " " . $row['last_name'],
-                "candidate_id" => $row["candId"]
+                "candidate_id" => $row["candId"],
+                "num_votes" => $row["num_votes"]
             );
         }
+        
+        
 
         echo json_encode($candidates); // Creating json response for voting sheet
         break;
@@ -71,56 +78,53 @@ switch ($action) {
     case 'cast_votes':
 
         //Cast votes
-
 //        $selectedPresident = $_GET['Presidents'];
 //        $sselectedSenetor = $_GET['Senators'];
 //        $selectedFr = $_GET['Faculty_Representatives'];
-        
+
         $user_id = $_GET['user_id'];
 //        print_r($_GET);
 
-        foreach ($_GET as $candidate_id) { //since number of votes which can be casted is dynamic so we have to loop na hizo id za candidates tunazipata kwenye hiyo for loop
-            if ($candidate_id !== 'cast_votes') {
-
-                //Get position id from provided candidate id
-                $query_get_candidate = "SELECT position_id
+        foreach ($_GET['votes'] as $candidate_id) { //since number of votes which can be casted is dynamic so we have to loop na hizo id za candidates tunazipata kwenye hiyo for loop
+//            if ($candidate_id !== 'cast_votes'  && $candidate_id !== $user_id) {
+            //Get position id from provided candidate id
+            $query_get_candidate = "SELECT position_id
                                           FROM candidates
                                          WHERE id = {$candidate_id}
                                          LIMIT 1";
 
-                $result_get_candidate = mysqli_query($link, $query_get_candidate) or die(mysqli_error($link));
+            $result_get_candidate = mysqli_query($link, $query_get_candidate) or die(mysqli_error($link));
 
-                if (mysqli_num_rows($result_get_candidate) == 1) {
-                    $row = mysqli_fetch_array($result_get_candidate);
-                    $position_id = $row['position_id'];
+            if (mysqli_num_rows($result_get_candidate) == 1) {
+                $row = mysqli_fetch_array($result_get_candidate);
+                $position_id = $row['position_id'];
 
-                    //CAST A VOTE
-                    $query_cast_vote = "INSERT INTO votes
+                //CAST A VOTE
+                $query_cast_vote = "INSERT INTO votes
                                                  (user_id, positon_id, election_period_id, candidate_id)
                                           VALUES ({$user_id}, '{$position_id}', 1, '{$candidate_id}')";
 
-                    $result_cast_vote = mysqli_query($link, $query_cast_vote) or die(mysqli_error($link));
+                $result_cast_vote = mysqli_query($link, $query_cast_vote) or die(mysqli_error($link));
 
-                    if ($result_cast_vote) {
+                if ($result_cast_vote) {
 
-                        //Votes casted succefully 
-                        $response = array(
-                            'cast_status' => 'success'
-                        );
-                    } else {
-                        //Can't cast vots
-                        $response = array(
-                            'cast_status' => 'failed'
-                        );
-                    }
+                    //Votes casted succefully 
+                    $response = array(
+                        'cast_status' => 'success'
+                    );
+                } else {
+                    //Can't cast vots
+                    $response = array(
+                        'cast_status' => 'failed'
+                    );
                 }
+//                }
             }
         }
 
         echo json_encode($response);
 
         break;
-        
 }
 ?>
 
